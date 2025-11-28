7d27929bdb597afaebae27ce6a12254d8fc5fa07
Nimordialgare <tumeilustras@gmail.com>
2025-11-28 19:29:29 +0000

    Refactor service worker cache management

--- sw.js
+++ sw.js
@@ -1,36 +1,18 @@
-const CACHE_NAME = "fichaje-obras-v1";
+const CACHE_NAME = "fichaje-brb-v1";
 const URLS_TO_CACHE = [
   "./",
   "./index.html",
   "./manifest.webmanifest"
-  // Si quieres, añade aquí CSS/JS adicionales si los separas
 ];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(URLS_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE))
   );
 });
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      // Devuelve de caché si está, si no, tira de red
-      return response || fetch(event.request);
-    })
-  );
-});
-
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(
-        keys
-          .filter((key) => key !== CACHE_NAME)
-          .map((key) => caches.delete(key))
-      )
-    )
+    caches.match(event.request).then((response) => response || fetch(event.request))
   );
 });
